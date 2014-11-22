3ce92dd155d6f239a5a2bdbd249e6aba5d63ecd7
Timothy Humphries <tim@utf8.me>
2014-11-22 16:44:13 -0600

    Simple skeleton

--- src/speedreader.user.js
+++ src/speedreader.user.js
@@ -7,4 +7,20 @@
 // @copyright  2014+, T. Humphries, S. Ruji
 // ==/UserScript==
 
-alert("AAAAAAAAAAAA");
+// Spawn floating button until we start to bundle
+var button = document.createElement("a");
+button.innerHTML = "Speed read selection";
+button.setAttribute("href", "#");
+button.setAttribute("onclick", "processSelection(); return false;")
+button.setAttribute("style", "position: fixed; right: 0; left: auto;");
+document.body.insertBefore(button, document.body.firstChild);
+
+function processSelection () {
+    var selObj = window.getSelection();
+    var range = selObj.getRangeAt(0);
+    speedRead(range.toString());
+}
+
+function speedRead(s) {
+    alert(s);
+}
