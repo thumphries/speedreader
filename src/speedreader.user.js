953c70e1d3e89a9aa49e817f047a284deb06a5a1
Timothy Humphries <tim@utf8.me>
2014-11-22 14:47:02 -0500

    Dummy article and skeleton

---
+++ src/speedreader.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name       Speed Reader
+// @namespace  http://github.com/shelf/speedreader
+// @version    0.1
+// @description  Fascinating speed reading tool
+// @match      http://*/*
+// @copyright  2014+, T. Humphries, S. Ruji
+// ==/UserScript==
+
+alert("AAAAAAAAAAAA");
