f539700088cf4ec2aaeb96baf3884f14e9e7e36d
Timothy Humphries <tim@utf8.me>
2014-11-22 18:02:52 -0600

    Simple faulty lightbox

--- src/speedreader.user.js
+++ src/speedreader.user.js
@@ -15,6 +15,8 @@ button.setAttribute("onclick", "processSelection(); return false;")
 button.setAttribute("style", "position: fixed; right: 0; left: auto;");
 document.body.insertBefore(button, document.body.firstChild);
 
+lightbox();
+
 function processSelection () {
     var selObj = window.getSelection();
     var range = selObj.getRangeAt(0);
@@ -22,5 +24,44 @@ function processSelection () {
 }
 
 function speedRead(s) {
-    alert(s);
+    $('#lb-content').html(s);
+    $('#lightbox').show();
+}
+
+function lightboxStyle() {
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML =
+	'#lightbox {' +
+	' position: fixed; top:0; left:0; width: 100%; height: 100%;' +
+	' background-color: black; text-align: center;' +
+	' display: none;' +
+	' overflow: visible;' +
+	'}' +
+	'#lb-content {' +
+	' max-width: 90%;' +
+	' color: white;' +
+	' white-space: pre-wrap;' +
+	' text-align: justify;' +
+	' margin-left: auto; margin-right: auto;' +
+	' overflow: scroll;' +
+	'}';
+    return css;
+}
+
+function lightboxOverlay() {
+    var lbdiv = document.createElement("div");
+    lbdiv.setAttribute("id", "lightbox");
+    var content = document.createElement("div");
+    content.setAttribute("id", "lb-content");
+    lbdiv.appendChild(content);
+    return lbdiv;
+}
+
+function lightbox () {
+    document.body.appendChild(lightboxStyle());
+    document.body.appendChild(lightboxOverlay());
+    $('#lightbox').on("click", function() {
+        $('#lightbox').hide();
+    });
 }
